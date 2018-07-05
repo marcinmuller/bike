1513bb71d2aec8d49fa5ee0894c8b4a18d28efcf
mm <mmuller@o2.pl>
2018-07-05 05:45:23 +0200

    2

--- src/test/java/pl/mmuller/bicycles/BicyclesApplicationTests.java
+++ src/test/java/pl/mmuller/bicycles/BicyclesApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class BicyclesApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void testBorrowBike() {
     }
 
 }
